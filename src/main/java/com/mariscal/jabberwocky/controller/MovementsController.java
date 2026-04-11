be26da4af2f84589d3e112404f1ed845909c87af
Armando Mariscal <armando.marisscal@gmail.com>
2026-04-11 17:35:43 -0600

    feat: implement service layer and refactor controller for dependency injection

--- src/main/java/com/mariscal/jabberwocky/controller/MovementsController.java
+++ src/main/java/com/mariscal/jabberwocky/controller/MovementsController.java
@@ -1,26 +1,24 @@
 package com.mariscal.jabberwocky.controller;
 
 import com.mariscal.jabberwocky.model.Movement;
-import com.mariscal.jabberwocky.repository.MovementRepository;
-
+import com.mariscal.jabberwocky.service.MovementService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/movements")
 public class MovementsController {
 
-    private final MovementRepository repository;
+    private final MovementService service;
 
-    public MovementsController(MovementRepository repository) {
-        this.repository = repository;
+    public MovementsController(MovementService service) {
+        this.service = service;
     }
 
     @GetMapping
     public List<Movement> getAll() {
-        return repository.findAll();
+        return service.findAll();
     }
 }
